546174ee624f477bc3e8e28d757285ebea3bbc92
Andraž De Luisa <andraz.de-luisa@student.fmf.uni-lj.si>
2018-11-12 12:53:16 +0100

    vaje

---
+++ resisistem.m
@@ -0,0 +1,13 @@
+function [x] = resisistem(A, b)
+    [L, U] = lubp(A);
+    y = zeros(n);
+    for i = 1:n
+        %prema substitucija
+    end
+    x = zeros(n);
+    for i = n:1:-1
+        %obratna substitucija
+    end
+
+end
+
