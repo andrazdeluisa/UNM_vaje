5e0763c6964a9f46795722b8a7cf28d343c8e472
Andraž De Luisa <andrazdl@gmail.com>
2018-12-02 16:53:30 +0100

    dn1

---
+++ DN1/premica.m
@@ -0,0 +1,13 @@
+function f = premica (A, v)
+    % premica skozi tocko A smernim vektorjem v
+    
+    x0 = A(1);
+    y0 = A(2);
+    vx = v(1);
+    vy = v(2);
+    
+    k = vy / vx;
+    n = y0 - k * x0;
+    
+    f = @(x) k * x + n;
+end
